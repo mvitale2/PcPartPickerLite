f94a070e99e7bd34fe0421f0a1734d68e6e26943
Skel <122138703+mvitale2@users.noreply.github.com>
2025-03-12 13:49:31 -0400

    supabase experimentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
 import './App.css'
+import { useState, useEffect } from 'react';
 import Header from './components/Header/Header'
+import supabase from './SupabaseClient.jsx';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const { data, error } = await supabase.from('CPU').select();
+
+    if (error) {
+      console.error("Error fetching data:", error)
+    } else {
+      setData(data);
+      console.log(`Retrieved data: ${data}`);
+    }
+  }
 
   function Body() {
+    const handleClick = (e) => {
+      e.preventDefault();
+      fetchData();
+    }
+
     return (
       <main>
         <h2>Build a new pc!</h2>
-        <button>Start</button>
+        <button onClick={handleClick}>Start</button>
+        <div>
+          <h3>Data from Supabase:</h3>
+          <ul>
+            {data.map((item) => (
+              <li key={item.id}>{item.model}</li>
+            ))}
+          </ul>
+        </div>
       </main>
     )
   }
